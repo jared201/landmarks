6bdae89f1486892e928232e873a8bbf24a1f94ae
Jared Odulio <jared.odulio@gmail.com>
2023-01-15 20:31:49 +0800

    initial commit

---
+++ Landmarks/Views/LandmarkList.swift
@@ -0,0 +1,31 @@
+//
+//  LandmarkList.swift
+//  Landmarks
+//
+//  Created by Jared Odulio on 1/15/23.
+//
+
+import SwiftUI
+
+struct LandmarkList: View {
+    var body: some View {
+        NavigationView{
+            List (landmarks){
+                landmark in
+                NavigationLink {
+                    LandmarkDetail()
+                } label: {
+                    LandmarkRow(landmark: landmark)
+                }
+            }
+            .navigationTitle("Landmarks")
+        }
+        
+    }
+}
+
+struct LandmarkList_Previews: PreviewProvider {
+    static var previews: some View {
+        LandmarkList()
+    }
+}
