21286890bc4aae08b8b407e4eb154fa28b8b01d4
Jared Odulio <jared.odulio@gmail.com>
2023-01-15 10:50:39 +0800

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Jared Odulio on 1/15/23.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
